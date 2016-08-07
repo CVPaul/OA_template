d2eeaaff5c7acd03f508dc21ebbc343ed05401be
CVPaul <xianqiu_li@163.com>
2016-08-07 16:00:20 +0800

    v0.1

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -34,59 +34,14 @@
 
 <meta charset="UTF-8">
 <link href="/OA_template/Public/css/slide/style.css" rel="stylesheet" type="text/css" />
-<meta charset="UTF-8">
-<link href="css/style.css" rel="stylesheet" type="text/css" />
-<div class="container">
-	<!-- progressbar -->
-	<section id="progressbar-slider" class="sample slider--animated" data-slidizle data-slidizle-loop="true" data-slidizle-timeout="3000" data-slidizle-pause-on-hover="true">
+<!-- css -->
+<link href="/OA_template/Public/css/bootstrap.min.css" rel="stylesheet" />
+<link href="/OA_template/Public/css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="/OA_template/Public/css/style.css" rel="stylesheet" />
 
-		<ul class="slider-content" data-slidizle-content>
-	
-			<li data-slidizle-slide-id="slide1" class="slider-item" style="background-image:url('/OA_template/Public/image/slide/01.jpg')">
-			</li>
-
-			<li data-slidizle-slide-id="slide2" data-slidizle-timeout="1000" class="slider-item" style="background-image:url('/OA_template/Public/image/slide/02.jpg')">
-			</li>
-
-			<li data-slidizle-slide-id="slide3" class="slider-item" style="background-image:url('/OA_template/Public/image/slide/03.jpg')">
-			</li>
-
-		</ul>
-
-		
-
-		<header>
-			<h2>介绍</h2>
-			<h3>这张图片的简单介绍</h3>
-		</header>
-
-		<div class="slider-next" data-slidizle-next>
-			<i class="fa fa-arrow-right"></i>
-		</div>
-		<div class="slider-previous" data-slidizle-previous>
-			<i class="fa fa-arrow-left"></i>
-		</div>
-
-		<div class="slider-progressbar"></div>
-
-		<ul class="slider-navigation" data-slidizle-navigation>
-			<li class="slider-navigation-item" data-slidizle-slide-id="slide2" style="background-image:url('/OA_template/Public/image/slide/02.jpg')">
-			</li>
-			<li class="slider-navigation-item" data-slidizle-slide-id="slide1" style="background-image:url('/OA_template/Public/image/slide/01.jpg')">
-			</li>
-			<li class="slider-navigation-item" data-slidizle-slide-id="slide3" style="background-image:url('/OA_template/Public/image/slide/03.jpg')">
-			</li>
-		</ul>
-
-		<div class="slidizle-loading">
-		</div>
-
-	</section>
-</div>
-
-<script src="http://libs.useso.com/js/jquery/2.1.1/jquery.min.js" type="text/javascript"></script>
-<script>window.jQuery || document.write('<script src="js/jquery-2.1.1.min.js"><\/script>')</script>
-<script src="js/jquery.slidizle.js"></script>
+<script src="/OA_template/Public/js/jquery.min.js" type="text/javascript"></script>
+<script>window.jQuery || document.write('<script src="/OA_template/Public/js/jquery-2.1.1.min.js"><\/script>')</script>
+<script src="/OA_template/Public/js/jquery.slidizle.js"></script>
 
 <script>
 
@@ -139,42 +94,144 @@
 		// });
 	});
 </script>
-<div id="<?php echo MODULE_NAME;?>" class="main"> 
+<div id="<?php echo MODULE_NAME;?>" class="main">
+	<div class="container">
+		<!-- progressbar -->
+		<section id="progressbar-slider" class="sample slider--animated" data-slidizle data-slidizle-loop="true" data-slidizle-timeout="3000" data-slidizle-pause-on-hover="true">
+
+			<ul class="slider-content" data-slidizle-content>
+		
+				<li data-slidizle-slide-id="slide1" class="slider-item" style="background-image:url('/OA_template/Public/image/slide/01.jpg')">
+				</li>
+
+				<li data-slidizle-slide-id="slide2" data-slidizle-timeout="1000" class="slider-item" style="background-image:url('/OA_template/Public/image/slide/02.jpg')">
+				</li>
+
+				<li data-slidizle-slide-id="slide3" class="slider-item" style="background-image:url('/OA_template/Public/image/slide/03.jpg')">
+				</li>
+
+			</ul>
+			<header>
+				<h2>介绍</h2>
+				<h3>这张图片的简单介绍</h3>
+			</header>
+
+			<div class="slider-next" data-slidizle-next>
+				<i class="fa fa-arrow-right"></i>
+			</div>
+			<div class="slider-previous" data-slidizle-previous>
+				<i class="fa fa-arrow-left"></i>
+			</div>
+
+			<div class="slider-progressbar"></div>
+
+			<ul class="slider-navigation" data-slidizle-navigation>
+				<li class="slider-navigation-item" data-slidizle-slide-id="slide2" style="background-image:url('/OA_template/Public/image/slide/02.jpg')">
+				</li>
+				<li class="slider-navigation-item" data-slidizle-slide-id="slide1" style="background-image:url('/OA_template/Public/image/slide/01.jpg')">
+				</li>
+				<li class="slider-navigation-item" data-slidizle-slide-id="slide3" style="background-image:url('/OA_template/Public/image/slide/03.jpg')">
+				</li>
+			</ul>
+
+			<div class="slidizle-loading">
+			</div>
+
+		</section>
+	</div>
 	<div id="new_notice" class="new_notice">
 		<div class="Journalism">
-			最新新闻
+			<h4>最新资讯</h4>
+			<ul>
+				<li><a href="#">Duis aute irure dolor in reprehenderit.</a><span>December 15, 2015</span></li>
+				<li><a href="#">Praesent vitae eros eget.</a><span>December 20, 2015</span></li>
+				<li><a href="#">rutrum sed sem quis venenatis.</a><span>December 25, 2015</span></li>
+				<li><a href="#">eros eget tellus tristique bibendum.</a><span>December 28, 2015</span></li>
+				<li><a href="#">excepturi sint occaecati.</a><span>January 3, 2016</span></li>
+			</ul>
 		</div>
-		<div class="Important_notice">
-			重要通知
+		<div class="Journalism">
+			<h4>重要通知</h4>
+			<ul>
+				<li><a href="#">Duis aute irure dolor in reprehenderit.</a><span>December 15, 2015</span></li>
+				<li><a href="#">Praesent vitae eros eget.</a><span>December 20, 2015</span></li>
+				<li><a href="#">rutrum sed sem quis venenatis.</a><span>December 25, 2015</span></li>
+				<li><a href="#">eros eget tellus tristique bibendum.</a><span>December 28, 2015</span></li>
+				<li><a href="#">excepturi sint occaecati.</a><span>January 3, 2016</span></li>
+			</ul>
 		</div>
 	</div>
 	<div id="department" class="department">
-		<div>部门</div>
-		<ul>
-			<li>部门1</li>
-			<li>部门2</li>
-			<li>部门3</li>
-			<li>部门4</li>
-		</ul>
+		<div class="skill-home"> <div class="skill-home-solid clearfix"> 
+		<div class="col-md-3 text-center">
+		<span class="icons c1"><i class="fa fa-trophy"></i></span> <div class="box-area">
+		<h3>Web Development</h3> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident</p></div>
+		</div>
+		<div class="col-md-3 text-center"> 
+		<span class="icons c2"><i class="fa fa-picture-o"></i></span> <div class="box-area">
+		<h3>UI Design</h3> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident</p></div>
+		</div>
+		<div class="col-md-3 text-center"> 
+		<span class="icons c3"><i class="fa fa-desktop"></i></span> <div class="box-area">
+		<h3>Interaction</h3> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident</p></div>
+		</div>
+		<div class="col-md-3 text-center"> 
+		<span class="icons c4"><i class="fa fa-globe"></i></span> <div class="box-area">
+		<h3>User Experiance</h3> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident</p>
+		</div></div>
+		</div></div>
 	</div>
 </div>
-
-
-		
-	<div class="common">
-		<div class="contact_us">
-			联系我们
+	<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="widget">
+					<h5 class="widgetheading">联系我们</h5>
+					<address>
+					<strong>Abovecompany Inc</strong><br>
+					JC Main Road, Near Silnile tower<br>
+					 Pin-21542 NewYork US.</address>
+					<p>
+						<i class="icon-phone"></i> (123) 456-789 - 1255-12584 <br>
+						<i class="icon-envelope-alt"></i> email@domainname.com
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<h5 class="widgetheading">快速链接</h5>
+					<ul class="link-list">
+						<li><a href="#">Latest Events</a></li>
+						<li><a href="#">Terms and conditions</a></li>
+						<li><a href="#">Privacy policy</a></li>
+						<li><a href="#">Career</a></li>
+						<li><a href="#">Contact us</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<h5 class="widgetheading">关注我们</h5>
+					<ul class="link-list">
+						<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+						<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+						<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<h5 class="widgetheading">合作伙伴</h5>
+					<ul class="link-list">
+						<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+						<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+						<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+					</ul>
+				</div>
+			</div>
 		</div>
-		<div class="fast_link">
-			快速链接
-		</div>
-		<div class="we_focus_on">
-			关注我们
-		</div>
-	</div>	
-	<div class="area footer">
-		Powered by © 2015 WINIT CORPORATION. All Rights Reserved.
 	</div>
-</div>
-</body>
-</html>
+	
+	<include file="Public:footer">
+	</footer>
